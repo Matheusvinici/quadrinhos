48f4a7a2325ba55a1e332ab8318671adb10c666e
Matheus Vinicius <matheus2vandrade@gmail.com>
2026-06-01 21:30:07 -0300

    Corrige nome da tabela no model RespostaAluno

--- app/Models/RespostaAluno.php
+++ app/Models/RespostaAluno.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RespostaAluno extends Model
 {
+    protected $table = 'respostas_aluno';
+
     protected $fillable = [
         'historia_id',
         'etapa',
